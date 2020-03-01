cae58c6aec9d850381116ca93b9422c9a02d9d52
SuLG-ik <nenashkinvov@gmail.com>
2020-03-01 23:57:35 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+  name = 'euclid',         
+  packages = ['euclid'],   
+  version = '0.1',      
+  license='LGPLv3+',        
+  description = 'modular inverse',   
+  author = 'Vova Nenashkin',                   
+  author_email = 'nenashkinvov@gmail.com',      
+  url = 'https://github.com/SuLG-ik/Euclid-Algorithm',   
+  download_url = 'https://github.com/SuLG-ik/Euclid-Algorithm/archive/master.zip',    
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',   
+    'Programming Language :: Python :: 3',      
+  ],
+)
